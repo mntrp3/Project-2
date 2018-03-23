73a4ad8467d80fe2dff0fe587dbf53693738e5b2
Benjamin Gordon <bogordon86@gmail.com>
2018-03-23 14:02:26 -0400

    git commit
    trying to get index2 map working

--- logic.js
+++ logic.js
@@ -4,7 +4,7 @@
 //renderMap(WeatherURL);
 
 //function renderMap(weatherURL) {}
-  d3.json("sevenday_filtered.json", function(response) {
+  d3.json("SevendayGeo.json", function(response) {
     console.log(response);
 
   // Loop through our data...
@@ -29,26 +29,30 @@
     function forEachForecast(data, layer) {
       layer.bindPopup("<h3>Temperature  " + temp + "<h3>Date:  " + day +","+ date + "<h3>Forecast:  " + longfore + "</h3>");
     };
+
+    var weather = L.geoJSON(data, {
+      onEachFeature: forEachForecastLayer,
+    });
     //Below closes out the for loop
       var timelineLayer = L.timeline(data, {
-        getInterval: function(data) {
+        style: function(data){
           return {
-            start: day + date,
-            end: day + date
-          };
-        },
-        pointToLayer: forEachForecastLayer,
-        onEachFeature: forEachForecast
+            pointToLayer: forEachForecastLayer,
+            onEachFeature: forEachForecast
+          }
+        }
       });
+
 // the above timeline layer code is now closed
-    };
-    createMap(timelineLayer);
-  });
+
+    createMap(weather, timelineLayer);
+  };
+});
 //Below closes out our d3 function that runs on the sevenday_filtered JSON data
 
 //createMap();
 //Adding function to create our map which is called above
-function createMap(timelineLayer) {
+function createMap(weather, timelineLayer) {
     var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
     "access_token=pk.eyJ1IjoiYm9nb3Jkb244NiIsImEiOiJjamViam45ZWIwZ3NhMnhsYWV2MXJscXBiIn0." +
     "vkP7IgE8o4eX6nIZeVtV0Q");
@@ -75,13 +79,13 @@ function createMap(timelineLayer) {
       layers: satellite
     });
 
-    L.control.layers(baseMaps, {
-      collapsed: true
-    }).addTo(map);
+    L.control.layers(baseMaps).addTo(map);
 
     var timelineControl = L.timelineSliderControl({
-      //formatOutput: function(date) {
-      //return new Date(date).toString();}
+      formatOutput: function(date) {
+            return new Date(date).toLocaleDateString();
+          },
+          enableKeyboardControls: true,
     });
     timelineControl.addTo(map);
     timelineControl.addTimelines(timelineLayer);
@@ -103,35 +107,8 @@ function resize(){
         $('#map').css("margin-top",-21);
     }
     //resizes the map after Bootstrap so it will fit the window
-    //map.invalidateSize();
+  //map.invalidateSize();
 }
 
-
-
-
-
-// // function createMap() {
-// // // Here we create a variable to store our url to mapbox to pull in the light map
-// //       var light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?" +
-// //       "access_token=pk.eyJ1IjoiYm9nb3Jkb244NiIsImEiOiJjamViam45ZWIwZ3NhMnhsYWV2MXJscXBiIn0.vkP7IgE8o4eX6nIZeVtV0Q");
-// //
-// //       var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
-// //       "access_token=pk.eyJ1IjoiYm9nb3Jkb244NiIsImEiOiJjamViam45ZWIwZ3NhMnhsYWV2MXJscXBiIn0.vkP7IgE8o4eX6nIZeVtV0Q");
-// // // Darkmap layer
-// //       var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
-// //       "access_token=pk.eyJ1IjoiYm9nb3Jkb244NiIsImEiOiJjamViam45ZWIwZ3NhMnhsYWV2MXJscXBiIn0." +
-// //       "vkP7IgE8o4eX6nIZeVtV0Q");
-// //
-
-// //
-// // // Creating map object
-//       var myMap = L.map("map", {
-//         center: [38.91, -77.036],
-//         zoom: 11,
-//         layers: outdoors
-//       });
-//
-//       L.control.layers(baseMaps).addTo(myMap);
-// }
 //this resizes the map after the initial load and fixes it
 resize();
